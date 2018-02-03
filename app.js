5dc475d60ebd60014400d6668156ee4a81d4aa9c
surekha <surekhasrao@gmail.com>
2018-02-03 17:27:33 +0000

    Udemy NodeJS and Express Lesson 2

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-//Find the version of Node on the c9 terminal
-//$ node --version
-//v6.11.2
+require('./instantHello');
+var goodbye = require('./talk/goodbye');
+var talk = require('./talk');
 
-//Find the version of npm
-//$ npm --version
-//3.10.10
+var question = require('./talk/question');
 
-//To run node on the console
-//$ node
-//> console.log("Hello World!")
-//Hello World!
-//undefined
-//> var name = "Surekha"
-//undefined
-//> console.log("Hello "+ name) 
-//Hello Surekha
-//ctrl+c twice to exit the node shell
+talk.intro();
+talk.hello('Surekha');
 
-var name = "Everyone";
-console.log("Hello " + name);
+var answer = question.ask("What is the meaning of Life?");
+console.log(answer);
+
+goodbye();
