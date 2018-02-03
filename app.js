0e918be84089438a1640b20947f4dd2dd875f425
surekha <surekhasrao@gmail.com>
2018-02-03 16:26:21 +0000

    Udemy NodeJS and Express Lesson 1

---
+++ app.js
@@ -0,0 +1,21 @@
+//Find the version of Node on the c9 terminal
+//$ node --version
+//v6.11.2
+
+//Find the version of npm
+//$ npm --version
+//3.10.10
+
+//To run node on the console
+//$ node
+//> console.log("Hello World!")
+//Hello World!
+//undefined
+//> var name = "Surekha"
+//undefined
+//> console.log("Hello "+ name) 
+//Hello Surekha
+//ctrl+c twice to exit the node shell
+
+var name = "Everyone";
+console.log("Hello " + name);
