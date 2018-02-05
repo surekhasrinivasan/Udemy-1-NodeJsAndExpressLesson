b5273c782d6bf29b23fb74612d43cd13c338ab10
surekha <surekhasrao@gmail.com>
2018-02-05 17:39:37 +0000

    Udemy NodeJS and Express Lesson 8

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var path = require('path');
 
 app.set('port', process.env.PORT);
 
-//app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(function(request, response,next){
+    console.log(request.method, request.url);
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/json', function(request, response){
     console.log("GET the json");
     response
