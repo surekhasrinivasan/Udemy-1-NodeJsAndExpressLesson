4f074044e54a1a034c172079bb1b947c06af3ea1
surekha <surekhasrao@gmail.com>
2018-02-14 17:42:08 +0000

    Section-4 upto lecture-43  Custom filters

--- Examples/Example-2/film/film-controller.js
+++ Examples/Example-2/film/film-controller.js
@@ -1,10 +1,10 @@
 angular.module('myApp').controller('FilmController', FilmController);
 
-function FilmController($http, $routeParams) {
+function FilmController($routeParams, FilmFactory) {
     var vm = this;
     var id = $routeParams.id;
-    $http.get('https://swapi.co/api/films/' + id).then(function(response){
+    FilmFactory.getOneFilm(id).then(function(response){
         //console.log(response.data);
-        vm.film = response.data;
+        vm.film = response;
     });
 }
--- Examples/Example-2/main/main-controller.js
+++ Examples/Example-2/main/main-controller.js
@@ -1,12 +1,18 @@
 angular.module('myApp').controller('MainController', MainController);
 
-function MainController($http) {
+function MainController(FilmFactory) {
     var vm = this;
     
-    $http.get('https://swapi.co/api/films/').then(function(response){
+    FilmFactory.getAllFilms().then(function(response){
         //console.log(response.data);
-        vm.films = response.data.results;
+        vm.films = response.results;
     });
     vm.name = 'Surekha';
+    
+    vm.date1 = '12 February 2016';
+    vm.date2 = '11 March 2016';
+    vm.date3 = '03 January 2015';
+    vm.date4 = '02 April 2014';
+    vm.date5 = '01 May 2016';
 }
 
